10b97689c2a85bf266551982d8f7a36571169de5
GabrielNavZap <gabrielnavarrete49585@gmail.com>
2023-09-21 19:23:41 -0600

    added cuadratic solvers

--- EjercicioLab2.py
+++ EjercicioLab2.py
@@ -7,7 +7,28 @@ def cubo () :
     
     print("\n"+base+" elevado al cubo es: "+resultado+"\n")
     
+def res_cuadr ():
+    a = input("Ingrese el coeficiente de x^2: ")
+    b = input("Ingrese el coeficiente de x: ")
+    c = input("Ingrese el termino independiente: ")
+    
+    if int(a)==0:
+        print("La ecuacion no es cuadratica")
+        
+    else:
+        resultado1 = (-int(b)+(((int(b)**2)-(4*int(a)*int(c)))**0.5))/(2*int(a))
+        resultado2 = (-int(b)-(((int(b)**2)-(4*int(a)*int(c)))**0.5))/(2*int(a))
+    
+        resultado1 = str(resultado1)
+        resultado2 = str(resultado2)
+        
+        print("Las racies de "+a+"x^2+"+b+"x+"+c+" son: ")
+        print(resultado1+" y "+resultado2)
+    
+sol1_cuad = lambda a,b,c: ((-b + ((b**2 - 4*a*c)**0.5)) / (2*a)) if a != 0 else None
 
+sol2_cuad = lambda a,b,c: ((-b - ((b**2 - 4*a*c)**0.5)) / (2*a)) if a != 0 else None
+    
 
 def mostrar_menu():
     print("1. Convierte cadena a Mayusculas (lambda)")
@@ -22,7 +43,20 @@ while True:
     mostrar_menu()
     opcion=input("Selecciona una opcion: ")
     
-    if opcion=="3":
+    if opcion=="1":
+        res_cuadr();
+        
+    elif opcion=="2":
+        a=input("Ingrese el coeficiente de x^2: ")
+        b=input("Ingrese el coeficiente de x: ")
+        c=input("Ingrese el termino independiente: ")
+        raiz1 = sol1_cuad(int(a),int(b),int(c))
+        raiz2 = sol2_cuad(int(a),int(b),int(c))
+        
+        print("Las soluciones para "+a+"x^2+"+b+"x+"+c+" son: ")
+        print(str(raiz1)+" y "+str(raiz2))
+        
+    elif opcion=="3":
         numero = input("Ingrese el numero del que desea calcular el cubo: ")
         resultado = cube(int (numero))
         numero = str(numero)
